5c713814882b2f0d6f6380c5835df6fff909eb45
Alhassan Mohamed <65092971+alhassanmohamed2@users.noreply.github.com>
2022-06-03 00:30:30 +0200

    Generate EDA report in html
    Still have some bugs and some improvement must be done

---
+++ EDA/eda.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from os import listdir, path
+import generate_html
+file = ""
+files_list =dict(enumerate([i for i in listdir() if path.splitext(i)[1] in ['.csv', '.xlsx']]))
+print(pd.DataFrame(files_list,index=range(1)).T)
+while True:
+    try:
+        file_number = int(input('Type the number of the file :'))
+        if file_number > len(files_list) -1 or file_number < 0:
+            print("You enterd number is out ranged")
+        else:
+            break
+    except ValueError:
+        print("Please Enter a Vaild Number Not a String")
+
+
+filename = files_list[file_number]
+
+if path.splitext(filename)[1] == ".csv":
+    file = pd.read_csv(filename)
+elif path.splitext(filename)[1] == ".xlsx":
+    file = pd.read_excel(filename)
+
+
+generate_html.ge(file, path.splitext(filename)[0])
---
+++ EDA/generate_html.py
@@ -0,0 +1,34 @@
+from jinja2 import Environment, FileSystemLoader
+import shutil
+
+
+def ge(data, filename):
+    original = './base_template.html'
+    target = f'./{filename}.html'
+
+    shutil.copyfile(original, target)
+
+    env = Environment(loader = FileSystemLoader('./') )
+    template = env.get_template(target)
+
+    with open(target, 'w') as file:
+        file.write(template.render(
+            filename = filename,
+            data_head = data.head().to_html(),
+            data_tail = data.tail().to_html(),
+            data_shape = data.shape,
+            data_size = data.size,
+            data_ndim = data.ndim,
+            data_describe = data.describe().to_html(),
+            data_sample = data.sample().to_html(),
+            data_isnull_sum = data.isnull().sum().to_frame().to_html(),
+            data_nunique = data.nunique(),
+            data_columns = data.columns,
+            data_memory_usage = data.memory_usage().to_frame().to_html(),
+            data_duplicated = data.duplicated().sum(),
+            # data_value_counts = data.value_counts().to_frame().to_html(),
+            # data_corr = data.corr().to_html(),
+            data_dtypes = data.dtypes.to_frame().to_html()   
+        ))
+    
+ 
